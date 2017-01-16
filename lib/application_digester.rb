9c08c61184f4b0b813d3d664356258f77dbd1ec8
Derek Schneider <dschneider.641@gmail.com>
2017-01-16 12:58:54 -0500

    Use relative paths instead of absolute paths

--- lib/application_digester.rb
+++ lib/application_digester.rb
@@ -4,10 +4,24 @@ class ApplicationDigester
   # Taking into account the location of every file helps capture changes such as
   # a file moving location, but the contents remain unchanged.
   def digest
-    Digest::SHA256.hexdigest(file_digests.join + paths.join)
+    Digest::SHA256.hexdigest(file_digests.join + relative_paths.join)
   end
 
   private
+
+  def relative_paths
+    paths.map do |path|
+      if path.start_with?(absolute_path_prefix)
+        path.split(absolute_path_prefix, 2).last
+      else
+        fail "Expected string to start with #{absolute_path_prefix}"
+      end
+    end
+  end
+
+  def absolute_path_prefix
+    Rails.root.to_path + '/' # Not sure if we need to add this.
+  end
 
   def file_digests
     files.map { |path| Digest::SHA256.hexdigest(File.read(path)) }
